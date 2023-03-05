02cbb47c66b50d3e19ebd57f6c70dc79fe7baed7
Harisankar <94229107+Methaphur@users.noreply.github.com>
2023-03-05 12:33:11 +0530

    Better representation

--- Expense_manager.py
+++ Expense_manager.py
@@ -1,65 +1,60 @@
-    from tabulate import tabulate
-    def instructions():
-        print(f''' 
-    A simple program to print a tabular form of transactions 
-    Input names of users in the transactions. Flow of money is from Left to Top 
-    i.e Y- axis has to pay X - axis correspondent ''')
-        print()
+from tabulate import tabulate
 
-    def users_init():
-        flag = True
-        users = []
-        while flag:
-            inp = input("Enter user name : ")
-            if inp != "":
-                users.append(inp)
+def instructions():
+    print(f''' 
+A simple program to print a tabular form of transactions 
+Input names of users in the transactions. Flow of money is from Left to Top 
+i.e Y- axis has to pay X - axis correspondent ''')
+    print()
+
+#  Inputs all the users in the transaction
+def users_init():
+    flag = True
+    users = []
+    while flag:
+        inp = input("Enter user name : ")
+        if inp != "":
+            users.append(inp)
+        else:
+            flag = False
+    
+    return users
+# Generating a board based off of user input
+def generate_board():
+    users = users_init()
+    board = [[0 for i in range(len(users))] for i in range(len(users))]
+    return board,users
+
+# Function to input payments
+def payments(board,users):
+    flag = True
+    while flag:
+        col = input("Enter who paid (User name): ")
+        if col != "":
+            row = input("Enter who all has to pay (User/All): ")
+            amount = int(input("Amount to be paid: "))
+            col = users.index(col)
+            if row != "All":
+               row = users.index(row)
+               board[row][col] += amount 
             else:
-                flag = False
-        
-        return users
+                for row in range(len(users)):
+                    if row != col:
+                        board[row][col] += amount
+            table_print(board,users)
+        else:
+            flag = False
 
-    def generate_board():
-        users = users_init()
-        board = [[0 for i in range(len(users))] for i in range(len(users))]
-        return board,users
+# Visually representing the Expenses
+def table_print(board,users):
+    visual_board = [row.copy() for row in board]
+    head = users[:]
+    for i in range(len(users)):
+        visual_board[i].insert(0,users[i])
+    head.insert(0," ")
+    print(tabulate(visual_board,headers=head,tablefmt="grid"))
 
-
-    def print_board(board,users):
-        print("  "*2 , end = "")
-        print(*users,sep = "   ")
-        i = 0
-        for row in board:
-            print(users[i],end = " ")
-            for item in row:
-                print(f'| {item} ' ,end = "")
-            print('|')
-            i += 1
-
-
-    def payments(board,users):
-        head = users
-        flag = True
-        while flag:
-            col = input("Enter who paid (User name): ")
-            if col != "":
-                row = input("Enter who all has to pay (User/All): ")
-                amount = int(input("Amount to be paid: "))
-                col = users.index(col)
-                if row != "All":
-                row = users.index(row)
-                board[row][col] += amount 
-                else:
-                    for row in range(len(users)):
-                        if row != col:
-                            board[row][col] += amount
-                print(tabulate(board,headers=head,tablefmt="grid"))
-            else:
-                flag = False
-    instructions()
-    board , users = generate_board()
-    # print_board(board,users)
-    print(tabulate(board,headers=users,tablefmt="grid"))
-    payments(board,users)
-    # print_board(board,users)
-    print(tabulate(board,headers=users,tablefmt="grid"))
-
+instructions()
+board , users = generate_board()
+table_print(board,users)
+payments(board,users)
